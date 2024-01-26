9047f233113ec7fee407c4ade4385e04f5e076e9
anatoly-mironov-pro <anatoly.mironov@ntprog.com>
2024-01-26 21:44:42 +0300

    Initial Commit

---
+++ PicturesApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PicturesApp
+//
+//  Created by Анатолий Миронов on 26.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PicturesApp/PicturesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicturesAppApp.swift
+//  PicturesApp
+//
+//  Created by Анатолий Миронов on 26.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct PicturesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
